c92a3513b4fa7292ef922e29b3efefa2db0ddf46
NRG <34063761+nehastats@users.noreply.github.com>
2022-01-25 19:34:09 +0530

    added comments

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 ## creating a list
+## initializing a list
 list=[]
 print("Blank list :")
 print(list)
@@ -7,7 +8,7 @@ list=[10,20,30]
 list1=["x","y","z"]
 print("\nList of numbers: ")
 print(list)
-
+# write a code which can represent a queue structure
 print(len(list))
 print("length of the list")
 
